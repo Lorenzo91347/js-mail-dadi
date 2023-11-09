aeebc0b0b26b8b950f9236fc2c008aebe211d581
Lorenzo91347 <lorenzo.losito91@gmail.com>
2023-11-09 17:59:34 +0100

    feat:js-mail done

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-#Dadi e Mail,
+# Dadi e Mail,
 
-due esercizi distinti
+due esercizi distinti:
+# Mail
+richiede di postare sulla console se una certa email e' autorizzata o meno.
+1. linkare in bottone dell'html a javascript.
+2. creare un array con 3 emails differenti.
+3. creare un EventListener con il click del bottone come trigger,
+con due condiziopnali per appaiare i contenuti dell'input con i contenuti dell'array 
+e produrre un console log differente a seconda dei contenuti dell'input.
--- js-mail/main.js
+++ js-mail/main.js
@@ -1,19 +1,17 @@
 'use strict'
-
+// id bottone e array di mails
 const mBtn = document.getElementById('btn')
-const aRray = ['pinco.pallino@gmail.com', 'ciccio.pasticcio@gmail.com', 'mario.carti@gmail.com'];
-let mail1 = aRray[0]
-let mail2 = aRray[1]
-let mail3 = aRray[2]
+const aRray = ["pinco.pallino@gmail.com", "ciccio.pasticcio@gmail.com", "mario.carti@gmail.com"];
+
+// event listener con recupero del valore dell'input e condizionali
 
 mBtn.addEventListener('click', function () {
     const eMail = document.getElementById('mail').value;
 
 
-    if (eMail.value === mail1) {
+    if (eMail === aRray[0]) {
         console.log('Accesso Autorizzato')
-        break;
-    } else if (eMail.value === mail2 || mail3) {
+    } else if (eMail === aRray[1] || aRray[2]) {
         console.log('Accesso Negato')
     };
 
